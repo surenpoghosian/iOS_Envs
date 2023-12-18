655b244e008275c521e4c709cf216b715f538f8c
surenpoghosian <56313895+surenpoghosian@users.noreply.github.com>
2023-12-18 21:49:24 +0400

    init

--- envTest/ViewController.swift
+++ envTest/ViewController.swift
@@ -5,13 +5,49 @@
 //  Created by Suren Poghosyan on 18.12.23.
 //
 
+#if DEVELOPMENT
+    let environment = "Development"
+#elseif STAGING
+    let environment = "Staging"
+#elseif PRODUCTION
+    let environment = "Production"
+#else
+    let environment = "Unknown"
+#endif
+
+
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let path = Bundle.main.path(forResource: environment, ofType: "plist") {
+            if let data = try? Data(contentsOf: URL(fileURLWithPath: path)) {
+                do {
+                    let plistData = try PropertyListSerialization.propertyList(from: data, options: [], format: nil)
+                    if let dict = plistData as? [String: Any] {
+                        // Access Plist data here
+                        if let name = dict["color"] as? String {
+                            print("Color: \(name)")
+                        }
+                    }
+                } catch {
+                    print("Error reading Plist: \(error)")
+                }
+            }
+        }
+        
+        switch environment {
+            
+        case "Development":
+            view.backgroundColor = .gray
+        case "Production":
+            view.backgroundColor = .green
+        default:
+            view.backgroundColor = .black
+        }
     }
 
 
